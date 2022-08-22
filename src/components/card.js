810cfc57dbf4e277cb9a51485ae33517595b0401
dineshdeveloper619 <dk4322350@gmail.com>
2022-08-22 13:05:13 +0530

    two

--- src/components/card.js
+++ src/components/card.js
@@ -5,7 +5,7 @@ const Card=()=>{
     return(
         <>
         <div className="card">
-          <h2>Details</h2>
+          <h2>Projects</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident ullam veniam porro inventore corrupti, voluptatibus nobis molestiae. Eius autem tempora ad sapiente fugit? Ut fuga doloremque nemo, quam fugiat tenetur?
       Temporibus maiores quibusdam reprehenderit repudiandae similique itaque quae quas, beatae reiciendis recusandae, officiis eveniet minima! Laudantium fugiat minima excepturi itaque delectus blanditiis ut voluptatum! Natus ad necessitatibus soluta asperiores atque.
     </p>
